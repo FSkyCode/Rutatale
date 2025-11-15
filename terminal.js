724f7a6219ca652b4f862bf9c4a31c1e4e77d591
TuNombre <juanfelipesierracastillo@gmail.com>
2025-11-14 22:37:01 -0500

    AYUDA

--- terminal.js
+++
@@ -1,7 +0,0 @@
-export const term = document.getElementById("terminal");
-export const input = document.getElementById("inputLine");
-
-export function print(text) {
-  term.innerText += text + "\n";
-  term.scrollTop = term.scrollHeight;
-}
