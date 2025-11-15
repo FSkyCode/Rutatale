5f9208ac19132048413f3fe3b33a49bd9adfd674
FSkyCode <juanfelipesierracastillo@gmail.com>
2025-11-14 22:23:40 -0500

    Actualizar main.js

--- main.js
+++ main.js
@@ -1,14 +1,12 @@
 import { input } from "./terminal.js";
 import { procesarEntrada } from "./events.js";
 
-document.addEventListener("DOMContentLoaded", () => {
-  input.addEventListener("keydown", function(event) {
-    if (event.key === "Enter") {
-      const texto = input.value.trim();
-      procesarEntrada(texto);
-      input.value = "";
-    }
-  });
-
-  input.focus();
+input.addEventListener("keydown", (event) => {
+  if (event.key === "Enter") {
+    const texto = input.value.trim();
+    procesarEntrada(texto);
+    input.value = "";
+  }
 });
+
+input.focus();
