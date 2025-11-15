55d8960e37747afd518c8c7cfa22eeb0fbadf65d
FSkyCode <juanfelipesierracastillo@gmail.com>
2025-11-14 21:50:35 -0500

    Crear terminal.js

---
+++ terminal.js
@@ -0,0 +1,7 @@
+export const term = document.getElementById("terminal");
+export const input = document.getElementById("inputLine");
+
+export function print(text) {
+  term.innerText += text + "\n";
+  term.scrollTop = term.scrollHeight;
+}
