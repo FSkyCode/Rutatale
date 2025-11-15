4ca410f68c3b9901898e787afdb2ff50127757e4
FSkyCode <juanfelipesierracastillo@gmail.com>
2025-11-14 22:07:34 -0500

    Actualizar events.js

--- events.js
+++ events.js
@@ -1,42 +1,37 @@
 import { print } from "./terminal.js";
-import {
-  estado, distancia, meta,
-  player, enemigoActual,
-  kills, spared
-} from "./game_state.js";
-
+import { state } from "./game_state.js";
 import { iniciarCombate, procesarCombate } from "./combat.js";
 import { generar_enemigo, jefe_final } from "./game_state.js";
 
 export function procesarEntrada(texto) {
-  if (estado === "intro") {
+  if (state.estado === "intro") {
     print("Rutatale for zfSkyCode");
     print("Presiona ENTER vacío para avanzar.\n");
-    estado = "caminar";
+    state.estado = "caminar";
     return;
   }
 
-  if (estado === "caminar") {
+  if (state.estado === "caminar") {
     if (texto !== "") {
       print("Solo usa ENTER vacío.");
       return;
     }
 
-    distancia++;
-    print(`➡ Avanzaste. Progreso: ${distancia}/${meta}`);
+    state.distancia++;
+    print(`➡ Avanzaste. Progreso: ${state.distancia}/${state.meta}`);
 
     if (Math.random() < 0.45) {
       iniciarCombate(generar_enemigo());
       return;
     }
 
-    if (distancia >= meta) {
+    if (state.distancia >= state.meta) {
       print("🔥 ¡HAS LLEGADO AL JEFE FINAL! 🔥");
       iniciarCombate(jefe_final());
     }
   }
 
-  if (estado === "combate") {
+  if (state.estado === "combate") {
     procesarCombate(texto);
   }
 }
